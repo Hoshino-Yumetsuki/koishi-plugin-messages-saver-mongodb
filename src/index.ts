bf7c79d41455f7edd5217a4e893b3f62259cfe2c
Hieuzest <girkirin@hotmail.com>
2024-02-22 05:46:54 +0800

    feat: basic impl

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Context, Schema } from 'koishi'
+import { Context, Primary, Schema } from 'koishi'
+
+declare module 'koishi' {
+  interface Tables {
+    '@hieuzest/messages': Message
+  }
+}
 
 export const name = 'messages'
 
@@ -7,5 +13,53 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.model.extend('@hieuzest/messages', {
+    id: 'primary',
+    messageId: 'string',
+    platform: 'string',
+    channelId: 'string',
+    guildId: 'string',
+    userId: 'string',
+    username: 'string',
+    content: 'string',
+    createdAt: 'timestamp',
+    updatedAt: 'timestamp',
+  })
+
+  ctx.on('message', async (session) => {
+    await ctx.database.create('@hieuzest/messages', {
+      messageId: session.messageId,
+      platform: session.platform,
+      channelId: session.channelId,
+      guildId: session.guildId,
+      userId: session.userId,
+      username: session.username,
+      content: session.content,
+      createdAt: new Date(session.timestamp),
+    })
+  })
+
+  ctx.on('message-updated', async (session) => {
+    await ctx.database.set('@hieuzest/messages', {
+      messageId: session.messageId,
+      platform: session.platform,
+      channelId: session.channelId,
+    }, {
+      content: session.content,
+      updatedAt: new Date(session.event.message.updatedAt ?? Date.now()),
+    })
+  })
+}
+
+interface Message {
+  id: Primary
+  messageId: string
+  platform: string
+  channelId: string
+  guildId: string
+  userId: string
+  username: string
+  content: string
+  createdAt: Date
+  updatedAt: Date
 }
